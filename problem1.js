c7b35e7526a8ff44fdc282a4cf08133280e9e9be
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-06-01 17:18:24 +0530

    This is the complete version

--- problem1.js
+++ problem1.js
@@ -1,37 +1,76 @@
-function problem1() {
-    const fs = require("fs");
-    fs.mkdir("./randomJSON", (error, data) => {
-        if (error) {
-            console.error(error);
-        } else {
-            console.log("directory named randomJSON is made");
+const fs = require("fs");
 
-            fs.writeFile("./randomJSON/1.txt", "hello from the other side", (error) => {
+function problem1() {
+    let dir = "./randomJSON/";
+
+    fs.access(dir, (error) => {
+        if (error) {
+            console.log(`Creating directory ${dir} as it does not exist.`);
+
+            fs.mkdir(dir, (error, data) => {
                 if (error) {
                     console.error(error);
-                } else {
-                    console.log("files written to the randomJSON directory");
+                }
 
-                    fs.writeFile("./randomJSON/2.txt", "hello from the other side 2", (error) => {
+                let allFileName = []
+
+                for (var i = 0; i < 5; i++) {
+                    let randomFileNumber = Math.floor(Math.random() * 10);
+                    let randomFileName = dir + "randomFile" + randomFileNumber + ".json";
+                    let jsonData = { message: "Hello, from the other side" };
+
+                    fs.access(randomFileName, (error) => {
                         if (error) {
-                            console.error(error);
-                        } else {
-                            console.log("files written to the randomJSON directory");
 
-                            fs.writeFile("./randomJSON/3.txt", "hello from the other side 3 ", (error) => {
+                            fs.writeFile(randomFileName, JSON.stringify(jsonData), "utf-8", (error) => {
                                 if (error) {
                                     console.error(error);
                                 } else {
-                                    console.log("files written to the randomJSON directory")
-                                    
+                                    allFileName.push(randomFileName);
+
+                                    console.log(`file written ${randomFileName}`);
+
+                                    deleteFiles(allFileName);
                                 }
                             });
+
                         }
+
                     });
 
                 }
+
             });
+        }
+        else {
+            console.log(`The ${dir} directory exists.`);
+
+            fs.rmdir(dir, { recursive: true }, (error) => {
+                if (error) {
+                    console.error(error);
+                } else {
+                    console.log(`deleted ${dir} including files inside`);
+                    console.log("Run again");
+                }
+            });
+
         }
     });
 }
 module.exports = problem1;
+
+function deleteFiles(allFile) {
+
+    allFile.forEach(file => {
+
+        fs.unlink(file, (error) => {
+            if (error) {
+                console.error(`${file} files doesnt exist`);
+            } else {
+                console.log(`file ${file} Deleted`);
+
+            }
+        });
+
+    });
+}
